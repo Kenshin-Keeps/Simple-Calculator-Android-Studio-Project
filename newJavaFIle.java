6d5571c626ef1cdd747f808043357d7e909eee47
Md. Zahim Hassan <zahim951xtgmovi@gmail.com>
2021-11-18 10:07:30 +0600

    New Java Class File Added

---
+++ newJavaFIle.java
@@ -0,0 +1,14 @@
+public class newjavaclass{
+    public float num;
+    newjavaclass(){
+        //COnstructor
+    }
+    public void setter(float num1){
+        this.num = num1;
+    }
+    public float getter(){
+        return this.num;
+    }
+}
+
+newjavaclass obj = new newjavaclass();
